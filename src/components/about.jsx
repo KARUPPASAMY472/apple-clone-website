f587c7ceb6d60fa776af7956ec035c6742aed0f5
KARUPPASAMY M <mkaruppas477@gmail.com>
2025-09-12 06:34:34 +0530

    apple clone oplk

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,9 +3,9 @@
 function About()
 {
     return (
-        <div>
-            <div className="bg-zinc-900 text-sm p-4 text-gray-400">
-            <div>
+        <div className="flex justify-center">
+            <div className="bg-zinc-900 text-sm p-4 md:p-20 flex justify-center flex-col justify-items-center  text-gray-400">
+            <div className="flex justify-center  flex-col">
                 <p className="py-2">
                     ◊ No Cost EMI is available with the purchase of an eligible product made using qualifying cards on 3- or 6-month tenures from most leading card issuers. Monthly pricing is rounded to the nearest rupee. Exact pricing will be provided by your card issuer, subject to your card issuer’s terms and conditions. Minimum order spend applies as per your card issuer’s threshold. No Cost EMI is not available to business customers and cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Card eligibility is subject to terms and conditions between you and your card issuer. Offer may be revised or withdrawn at any time without any prior notice. Terms apply.
                 </p>
@@ -41,6 +41,124 @@ function About()
 
                 </div>
                 
+                <div class="hidden md:flex  text-gray-300 justify-between gap-3 py-4">
+                    <div>
+                        <div  class="flex flex-col gap-4">
+                        <div>
+                            <h1 class="pb-2 text-white">Shop and Learn</h1>
+                            <ul class="flex flex-col  gap-2">
+                                <li>Store</li>
+                                <li>Mac</li> 
+                                <li>iPad</li> 
+                                <li>iPhone</li> 
+                                <li>Watch</li> 
+                                <li>AirPods</li> 
+                                <li>TV & Home</li> 
+                                <li>AirTag</li> 
+                                <li>Accessories</li> 
+                                <li>Gift Cards</li> 
+                            </ul>
+                            </div>
+                          <div>  
+                            
+                        <h1 class="pb-2 text-white">Account</h1>
+                        <p>Wallet</p> 
+                        </div>
+                            
+                        </div></div>
+                    
+                        <div  class="flex flex-col gap-4">
+                            <div>
+                        <h1 class="pb-2 text-white">Account</h1>
+                            <ul  class="flex flex-col  gap-2">
+                                <li>Manage Your Apple Account</li>
+                                <li>Apple Store Account</li> 
+                                <li>iCloud.com</li> 
+                                <li>iPhone</li>
+                        </ul>
+                        </div>
+                        
+                        <div>
+                            <h1 class="pb-2 text-white">Entertainment</h1>
+                            <div  class="flex flex-col  gap-2">
+                            <p>Apple One</p>
+                            <p>Apple TV+</p>
+                            <p>Apple Music</p>
+                            <p>Apple Arcade</p>
+                            <p>Apple Books</p>
+                            <p>App store</p>
+                            </div>
+                        </div>
+                        </div>
+                    
+                    <div  class="flex flex-col  gap-4">
+                        <h1 class="pb-2 text-white">Apple Store</h1>
+                        <div class="flex flex-col  gap-2">
+                            <p>Find a Store</p>
+                            <p>Genius Bar</p>
+                            <p>Today at Apple</p>
+                            <p>Apple Camp</p>
+                            <p>Apple Trade In</p>
+                            <p>Ways to Buy</p>
+                        <p>Recyling Programme</p>
+                        <p>Order Status</p>
+                        <p>Shopping Help</p>
+                    </div></div>
+                
+                <div class="flex flex-col  gap-4">
+                    <div>
+                    <h1 class="pb-2 text-white">For Business</h1>
+                    <div class="flex flex-col  gap-2">
+                    <p>Apple and Business</p>
+                    <p>Shop and Business</p>
+                    </div>
+                    </div>
+
+                    <div>
+                        <h1 class="pb-2 text-white">For Education</h1>
+                        <div  class="flex flex-col  gap-2">
+                        <p>Apple and Education</p>
+                        <p>Shop for Education</p>
+                        <p>Shop for University</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h1 class="pb-2 text-white">For Healthcare</h1>
+                        <p>Apple and Healthcare</p>
+                    </div>
+                    <div>
+                        <h1 class="pb-2 text-white">For Goverment</h1>
+                        <p>Apple and Goverment</p>
+                    </div>
+ 
+                </div>
+
+                <div class="flex flex-col  gap-4">
+                    <div>
+                        <h1 class="pb-2 text-white">Apple Values</h1>
+                        <div class="flex flex-col  gap-2" >
+                        <p>Accessibility</p>
+                        <p>Education</p>
+                        <p>Enviroment</p>
+                        <p>Privacy</p>
+                        <p>Supply Chain Innovation</p>
+                        </div>
+                        </div>
+                    <div>
+                        <h1 class="text-white pb-2">About Apple</h1>
+                        <div class="flex flex-col  gap-2">
+                        <p>Newsroom</p>
+                        <p>Apple Leadership</p>
+                        <p>Career Opertunities</p>
+                        <p>Investors</p>
+                        <p>Ethics & Compliance</p>
+                        <p>Events</p>
+                        <p>Contact Apple</p>
+                        </div>
+                    </div>
+                </div>
+
+                </div>
 
                 <div className="my-4 cursor-pointer md:hidden">
                     
@@ -106,7 +224,7 @@ function About()
 
                 <p className="my-2 text-center text-gray-300">India</p>
 
-                <div className="my-2">
+                <div className="my-2 justify-items-center">
                     <p className="text-center">Copyright © 2025 Apple Inc. All rights reserved.</p>
                         <div className="my-2 text-white flex justify-center items-center text-center md:gap-3">
                             <p className="cursor-pointer hover:underline">Privacy Policy</p> |
